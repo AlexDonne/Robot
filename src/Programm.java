761e73e4d675072f352d00a51563a90a9dc2ca95
Victor Bros <brosv@ensipc470.ensimag.fr>
2018-11-06 16:25:14 +0100

    add maze

--- src/Programm.java
+++ src/Programm.java
@@ -5,6 +5,6 @@ import java.awt.*;
 
 public class Programm {
     public static void main(String[] args) {
-        Simulateur simulateur = new Simulateur(9, "cartes/mushroomOfHell-20x20.map");
+        Simulateur simulateur = new Simulateur(9, "cartes/mazeOfFate-11x11.map");
     }
 }
